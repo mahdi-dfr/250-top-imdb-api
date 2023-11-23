09f270111505fb1a7a8c0540e5a5a6bdef3be3c5
amirhossein <amirhosseinmobini9@gmail.com>
2023-11-23 21:18:15 +0330

    movies registered in django admin panel

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
-# Register your models here.
+from movie import models
+
+
+admin.site.register(models.Actors)
+admin.site.register(models.AgeLimits)
+admin.site.register(models.Comments)
+admin.site.register(models.Directors)
+admin.site.register(models.Countries)
+admin.site.register(models.Genres)
+admin.site.register(models.Platform)
+admin.site.register(models.MoviesInfo)
+admin.site.register(models.SeriesInfo)
